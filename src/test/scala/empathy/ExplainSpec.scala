4092dc974c436bf1e1fd17744e68db7dfdeb55c6
salim <slpg500a>
2016-10-29 13:08:17 +0100

    Add a additional test for the explain behavior

--- src/test/scala/empathy/ExplainSpec.scala
+++ src/test/scala/empathy/ExplainSpec.scala
@@ -9,14 +9,25 @@ import org.scalatest.{FlatSpec, Matchers}
 class ExplainSpec extends FlatSpec with Matchers {
 
   "Explainr" should "be able to group scores" in {
-    val w:Weightings = Map("A"->List(1.0), "B"->List(1.0))
+    val w:Weightings = Map("A"->List(0.0, 1.0), "B"->List(0.0, 1.0))
     val g = Map("xx" -> Set("A", "B"))
     val row = Map("A"->2.0, "B"->3.0)
 
     val rf = new RankingFitness((strings: List[String]) =>0.0)
 
     val result:Map[String,Double] = rf.explainRows(row=row, weightings=w, explainMap=g)
+    assert(result==Map("xx"->5.0))
+  }
 
+  it should "be able to group scores into multiple groups" in {
+    val w:Weightings = Map("A"->List(0.0, 1.0), "B"->List(0.0, 1.0))
+    val g:Map[String,Set[String]] = Map("xx" -> Set("A", "B"), "yy" -> Set())
+    val row:Map[String,Double] = Map("A" -> 2.0, "B" -> 3.0)
+
+    val rf = new RankingFitness((strings: List[String]) =>0.0)
+
+    val result:Map[String,Double] = rf.explainRows(row=row, weightings=w, explainMap=g)
+    assert(result==Map("xx"->5.0, "yy"->0.0))
   }
 
 
